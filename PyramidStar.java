16359c415e787b1e73debde73068f427b5d2318f
rupeshsawantcv <rupeshsawantcv@gmail.com>
2025-11-13 12:37:51 +0530

    PyramidStar.java

---
+++ PyramidStar.java
@@ -0,0 +1,23 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class PyramidStar {
+    public static void main(String[] args) {
+        int n = 10;
+        for(int i =0; i<10;i++){
+            
+            
+            for(int j =1; j<n-i;j++){
+                System.out.print(" ");
+            }
+            
+            for(int k =0; k<=i;k++){
+                System.out.print("*");
+            }
+            for(int k =1; k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
